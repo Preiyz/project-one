3753b752c80234144acd525f33d2b9901a78f1aa
Ubuntu <ubuntu@ip-172-31-5-91.ec2.internal>
2023-03-22 00:39:20 +0000

    List of Commands

---
+++ command.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+#Create a file with the name domino.yaml
+touch domino.yaml
+
+#Create a directory with the name odsl-gxp
+mkdir odsl-gxp
+
+#Create a file with the name calender
+touch calender
+
+#Print out 3 calender months (previous current and next month) and redirect the output into the file calender 
+cal -3 > calender
+
+#Update the packages on the machine
+sudo apt-get update
+
+#Print out Job Processing.......
+echo "Job Processing......."
+
+#Let the machine sleep for 10 seconds
+sleep 10
+
+#Echo the sentence "YAML means Yaml Ain't Markup Language" and redirect it into domino.yaml
+echo "YAML means Yaml Ain't Markup Language" > domino.yaml
+
+#Echo "These are the last 5 users created on this machine" and append into domino.yaml
+echo "These are the last 5 users created on this machine" >> domino.yaml
+
+#To check the last 5 users on this machine and append into domino.yaml
+tail -5 /etc/passwd >> domino.yaml
+
+#Sleep for 5 seconds
+sleep 5
+
+#How do you clear an output
+clear
+
+#Print out Job sucessful
+echo "Job sucessful"
