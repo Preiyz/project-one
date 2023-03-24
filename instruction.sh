587cbaa5df92761ba2d780c1d6695142d3b8b616
Ubuntu <ubuntu@ip-172-31-5-91.ec2.internal>
2023-03-24 01:03:02 +0000

    New files added

---
+++ instruction.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#OWNER: PRAISE AKANBI
+#DATE CREATED: MARCH 23 2023
+#DESCRIPTION: Introduction into the automation of a linux machine using bash shell script.
+#This file will contain various linux commands and test run bash scripting
+
+#This command prints the following text
+echo "Hi there! I hope you're doing well today. Welcome to the playground..."
+echo "HA HA HA!!!"
+
+sleep 5
+echo "The current user is"
+#This command reveals the current user signed into this machine
+whoami
+sleep 3
+
+echo "You are currently working in"
+#This command reveals the present working directory
+pwd
+sleep 3
+
+echo "The date is"
+#This command shows the date
+date
+sleep 3
+
+echo "This machine has ran for"
+#This command shows the running time of the machine
+uptime
+sleep 3
+clear
+
+
+
